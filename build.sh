b0f4d4484f20edfd00ae5b1d8af7b6971bc911b7
Wiliam-L <wlopezguerra2000@gmail.com>
2024-09-30 00:20:51 -0600

    descomment pip install

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # Exit on error
 set -o errexit
 
-#pip install -r requirements.txt
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.get_or_create(username='wlopez', defaults={'email': 'admin@example.com', 'password': '123asdfgna'})" | python manage.py shell
