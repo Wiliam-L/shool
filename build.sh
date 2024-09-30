73263a3069e9b41e6f8780f39beb415a7a2e8992
Wiliam-L <wlopezguerra2000@gmail.com>
2024-09-30 00:52:23 -0600

    create groups and useradmin

--- build.sh
+++ build.sh
@@ -2,8 +2,30 @@
 # Exit on error
 set -o errexit
 
+# Instalar dependencias
 pip install -r requirements.txt
+
+# Recolectar archivos estáticos
 python manage.py collectstatic --no-input
+
+# Ejecutar migraciones
 python manage.py migrate
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.get_or_create(username='wlopez', defaults={'email': 'admin@example.com', 'password': '123asdfgna'})" | python manage.py shell
+# Crear el grupo y el usuario
+echo "
+from django.contrib.auth.models import User, Group
+
+# Crea el grupo
+group, created = Group.objects.get_or_create(name='admin')
+
+user, created = User.objects.get_or_create(
+    username='wlopez',
+    defaults={'email': 'admin@example.com'}
+)
+
+if created:
+    user.set_password('123asdfgna')
+    user.save()
+
+user.groups.add(group)
+" | python manage.py shell
