68c1a5129ebf3e6022c4ccaf86c2e3422920105e
Wiliam-L <wlopezguerra2000@gmail.com>
2024-10-10 22:02:36 -0600

    correction section

--- apps/administrator/urls.py
+++ apps/administrator/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import StudentViewSet, StudentShortListApiView, StudentNoteViewSet, RegistrationStudentViewSet, TeacherViewSet, SpecialityViewSet, TutorViewSet, CourseViewSet, LevelViewSet, GradeViewSet
-from section.views import SectionViewSet
+
 router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
 router.register(r'student', StudentViewSet, basename='student')
@@ -11,11 +11,12 @@ router.register(r'teacher', TeacherViewSet, basename='teacher')
 router.register(r'speciality', SpecialityViewSet, basename='speciality')
 router.register(r'tutor', TutorViewSet, basename='tutor')
 router.register(r'level', LevelViewSet, basename='level')
-router.register(r'section', SectionViewSet, basename='section')
+
 
 urlpatterns = [
     path('', include('apps.authentication.urls')),
     path('short-student', StudentShortListApiView.as_view(), name="short-student"),
+    path('section/', include('apps.section.urls')),
     path('', include(router.urls))
 ]
 
